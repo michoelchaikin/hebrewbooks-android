3d658c15db70355056f377c74ccbccbe27fd7f9d
michoelchaikin <micholi@gmail.com>
2013-04-29 19:36:51 +1000

    Improved link handling code
    * Properly handles links with or without leading "www"
    * Handles regular page links, and links to actual pdf file

--- src/com/michoelchaikin/hebrewbooks/ViewBookActivity.java
+++ src/com/michoelchaikin/hebrewbooks/ViewBookActivity.java
@@ -1,6 +1,8 @@
 package com.michoelchaikin.hebrewbooks;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -30,72 +32,108 @@ public class ViewBookActivity extends Activity {
 
 	private static final String TAG = "ViewBookActivity";
 	private static final int TEST_BOOK_ID = 15860;
-	
+
 	private HebrewBook mBook = null;
 	private volatile PageCacheManager mCacheManager = null;
-	
+
 	private int mCurrentPage = 0;
 	private Button mButPrev;
 	private Button mButNext;
 	private PageView mPageView;
 	private TextView mTextPage;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+
 		// Make sure we have Internet connection
-        
+
 		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo netInfo = cm.getActiveNetworkInfo();
-        if (netInfo == null || netInfo.isConnected() == false) {
-        	Toast.makeText(this, "Internet connection not available", Toast.LENGTH_LONG).show();
-        	finish();
-        }
-        
-        // Check Intent to see what book to open
-        
-        int bookID = 0;
-        final Intent intent = getIntent();
-        final String action = intent.getAction();
-        if (Intent.ACTION_VIEW.equals(action)) {
-        	Uri data = intent.getData();
-        	String strBookID = data.getQueryParameter("req");     	
-        	String strPageNumber = data.getQueryParameter("pgnum");
-        	bookID = HebrewBooksUtils.parseIntNoException(strBookID);
-        	mCurrentPage = HebrewBooksUtils.parseIntNoException(strPageNumber);
-        	
-        	if(bookID == 0) {
-        		Toast.makeText(this, "Could not open book: No book request found", Toast.LENGTH_LONG).show();
-        		finish();
-        	}
-        	
-        	if(mCurrentPage == 0) mCurrentPage = 1;
-        	
-        } else {
-        	// Started from app launcher
-        	bookID = TEST_BOOK_ID;
-    		mCurrentPage = 25;
-        }
-        
-        // UI Stuff
-        
+		NetworkInfo netInfo = cm.getActiveNetworkInfo();
+		if (netInfo == null || netInfo.isConnected() == false) {
+			Toast.makeText(this, "Internet connection not available", Toast.LENGTH_LONG).show();
+			finish();
+		}
+
+		createBook();
+
+		// UI Stuff
+
 		setContentView(R.layout.activity_view_book);
-					
-		mBook = new HebrewBook(this, bookID);
+
 		new InitBook().execute();
-		
+
 		mButPrev = (Button) findViewById(R.id.butPrev);
 		mButNext = (Button) findViewById(R.id.butNext);
 		mPageView = (PageView) findViewById(R.id.pageView);
 		mTextPage = (TextView) findViewById(R.id.textPage);
-		
+
 		mButNext.setEnabled(false);
 		mButPrev.setEnabled(false);
 	}
-	
+
+	private void createBook() {
+
+		/* The application can handle two types of URLS:
+		 * 
+		 *  1) http://www.hebrewbooks.org/pdfpager.aspx?req=[BOOK_ID]&pgnum=[BOOK_ID]
+		 *  2) http://www.hebrewbooks.org/pagefeed/hebrewbooks_org_[BOOK_ID]_[BOOK_ID].pdf
+		 * 
+		 * The first is the usual address used for viewing books on the site
+		 * The seconds is the address of the actual PDF file - it is usefull to handle such
+		 * links because they can come up in search results.
+		 * 
+		 */
+
+		int bookID = 0, page = 0;
+		final Intent intent = getIntent();
+		final String action = intent.getAction();
+
+		if (Intent.ACTION_VIEW.equals(action)) {
+			Uri data = intent.getData();
+			String path = data.getPath();
+
+			// Try first type of URL
+			if(path.startsWith("/pdfpager.aspx")) {        	
+				bookID = HebrewBooksUtils.parseIntNoException(data.getQueryParameter("req"));
+				page = HebrewBooksUtils.parseIntNoException(data.getQueryParameter("pgnum"));
+			}
+
+			// Try the second type of URL
+			else if (path.startsWith("/pagefeed/")) {
+				Pattern pattern = Pattern.compile("^/pagefeed/hebrewbooks_org_([\\d]*)_([\\d])*\\.pdf.*$");
+				Matcher matcher = pattern.matcher(path);
+				if(! matcher.matches()) {
+					Toast.makeText(this, "Could not open book: URL not understood", Toast.LENGTH_LONG).show();
+					finish();
+				}
+				bookID = HebrewBooksUtils.parseIntNoException(matcher.group(1));
+				page = HebrewBooksUtils.parseIntNoException(matcher.group(2)); 
+			} 
+
+			// Unrecognized URL type
+			else {
+				Toast.makeText(this, "Could not open book: Unrecognized URL", Toast.LENGTH_LONG).show();
+				finish();
+			}
+		} else if (Intent.ACTION_MAIN.equals(action)) {
+			// Started from app launcher
+			bookID = TEST_BOOK_ID;
+			page = 25;
+		}
+
+		if(bookID == 0) {
+			Toast.makeText(this, "Could not open book: No book request found", Toast.LENGTH_LONG).show();
+			finish();
+		}
+		if(page == 0) page = 1;
+
+		mCurrentPage = page;
+		mBook = new HebrewBook(this, bookID);
+	}
+
 	class InitBook extends AsyncTask<Void, Void, Boolean> {
-		
+
 		@Override
 		protected Boolean doInBackground(Void... params) {
 			try {
@@ -106,25 +144,25 @@ public class ViewBookActivity extends Activity {
 			}
 			return true;
 		}
-		
+
 		@Override
 		protected void onPostExecute(Boolean result) {
 			super.onPostExecute(result);
-			
+
 			if(result != true) {
 				Toast.makeText(ViewBookActivity.this, "Error reading book information. Please ensure book is valid and Internet connection available", Toast.LENGTH_LONG).show();
-	        	finish();
+				finish();
 			}
-			
+
 			setTitle(mBook.getNameHebrew() + " (" + mBook.getAuthorHebrew() + ")");
-			
+
 			mCacheManager = new PageCacheManager(mBook);
 			mCacheManager.init();
-			
+
 			loadPage(mCurrentPage);
 		}
 	}
-	
+
 	private void loadPage(int page) {
 
 		if(mBook == null || page < 1 || page > mBook.getNumPages()) {
@@ -141,11 +179,11 @@ public class ViewBookActivity extends Activity {
 	public void butPrev_onClick(View v) {
 		loadPage(mCurrentPage-1);
 	}
-	
+
 	public void butNext_onClick(View v) {
 		loadPage(mCurrentPage+1);
 	}
-	
+
 	public void textPage_onClick(View v) {			 
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
 		builder.setTitle("Go to page");
@@ -171,8 +209,8 @@ public class ViewBookActivity extends Activity {
 		Dialog dialog = builder.create();
 		dialog.show();
 	}
-	 
-	
+
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
